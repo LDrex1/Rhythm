cd2225c11d03effa366ea7d72e99a3f387dee268
lDrex1 <damzealwini7@gmail.com>
2022-10-26 00:55:16 +0100

    new component

---
+++ src/components/navigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function navigation() {
+  return (
+    <div>
+      <nav>
+        <span>
+          <img></img>
+        </span>
+
+        <div></div>
+      </nav>
+    </div>
+  );
+}
+
+export default navigation;
