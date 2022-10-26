affd8bd56acfe601c0059569a92738dae86e1a28
lDrex1 <damzealwini7@gmail.com>
2022-10-26 12:54:20 +0100

    new component

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Toolbar from "@mui/material";
+import Link from "@mui/material";
+import NavLinks from "./NavLinks";
 
 function navigation() {
   return (
-    <div>
-      <nav>
-        <span>
-          <img></img>
-        </span>
-
-        <div></div>
-      </nav>
-    </div>
+    <>
+      <Toolbar>
+        {NavLinks.map((link) => (
+          <Link key={link.title} color="inherit" noWrap href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+      </Toolbar>
+    </>
   );
 }
 
