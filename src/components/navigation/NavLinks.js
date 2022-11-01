1bc8d10526ff1fc6d6d17327a1e9f96f698924b7
lDrex1 <damzealwini7@gmail.com>
2022-11-01 15:25:25 +0100

    information on navigation links

---
+++ src/components/navigation/NavLinks.js
@@ -0,0 +1,9 @@
+const NavLinks = [
+  { title: "Home", url: "#" },
+  { title: "Tech", url: "#" },
+  { title: "About", url: "#" },
+  { title: "Contact", url: "#" },
+  { title: "Help", url: "#" },
+];
+
+export default NavLinks;
