7f3d475bde5d824bca43ae291e9378e21f395000
lDrex1 <damzealwini7@gmail.com>
2022-11-01 15:24:52 +0100

    directory change

--- src/components/NavLinks.js
+++
@@ -1,9 +0,0 @@
-const NavLinks = [
-  { title: "Home", url: "#" },
-  { title: "Tech", url: "#" },
-  { title: "About", url: "#" },
-  { title: "Contact", url: "#" },
-  { title: "Help", url: "#" },
-];
-
-export default NavLinks;
