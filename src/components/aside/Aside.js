a7a8cb9f9f71e7310fca1e21adc2ba97efb84e30
lDrex1 <damzealwini7@gmail.com>
2022-11-05 14:53:20 +0100

    update: anchor state and Drawer props added comp: List

--- src/components/aside/Aside.js
+++ src/components/aside/Aside.js
@@ -1,15 +1,33 @@
-import { Drawer, SwipeableDrawer } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Button, SwipeableDrawer } from "@mui/material";
+import List from "./AsideLinks";
+import styleAside from "../style/StyleAside";
 
 function Aside() {
+  const [anchor, setAnchor] = useState(false);
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setAnchor(open);
+  };
   return (
     <>
+      <Button onClick={toggleDrawer(true)}>Swipe</Button>
       <SwipeableDrawer
+        sx={styleAside.swiper}
         anchor={"left"}
-        open={true}
-        onClose={toggleDrawer(anchor, false)}
+        hideBackdrop={true}
+        open={anchor}
+        onClick={toggleDrawer(false)}
+        onOpen={toggleDrawer(true)}
+        onClose={toggleDrawer(false)}
       >
-        {list(anchor)}
+        <List />
       </SwipeableDrawer>
     </>
   );
