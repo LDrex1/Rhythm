5ecd091740e18b2ad671f0d48d37728f13b146d6
lDrex1 <damzealwini7@gmail.com>
2022-11-05 14:53:56 +0100

    new comp: information on aide links and List comp

---
+++ src/components/aside/AsideLinks.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, Divider, List, ListItem } from "@mui/material";
+import { ListItemButton, ListItemIcon, ListItemText, Box } from "@mui/material";
+import { Facebook, Instagram, Twitter, YouTube } from "@mui/icons-material";
+import styleAside from "../style/StyleAside";
+
+const asideLinks = [
+  { label: "Facebook", icon: <Facebook />, url: "#" },
+  { label: "Instagram", icon: <Instagram />, url: "#" },
+  { label: "YouTube", icon: <YouTube />, url: "#" },
+  { label: "Twitter", icon: <Twitter />, url: "#" },
+];
+
+const ListBox = () => (
+  <>
+    <Box sx={styleAside.box}>
+      <List>
+        {asideLinks.map(({ label, icon, url }, index) => (
+          <>
+            <ListItem key={label}>
+              <ListItemButton LinkComponent="a" href={url}>
+                <ListItemIcon sx={{ color: "white" }}>{icon}</ListItemIcon>
+                <ListItemText primary={label} sx={styleAside.itemButton} />
+              </ListItemButton>
+            </ListItem>
+            {index != asideLinks.length - 1 ? (
+              <Divider
+                sx={{
+                  background: "#ffffff",
+                  width: "30%",
+                  height: "0.1px",
+                  margin: "0 auto",
+                }}
+              />
+            ) : null}
+          </>
+        ))}
+      </List>
+    </Box>
+  </>
+);
+export default ListBox;
