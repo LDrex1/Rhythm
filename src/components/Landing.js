c77f31481ca4a3fdb5e74946ef98653662d493b1
lDrex1 <damzealwini7@gmail.com>
2022-11-05 14:51:54 +0100

    added comp: Aside

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Aside from "./aside/Aside";
 import Navigation from "./navigation/Navigation";
 
 function Landing() {
   return (
     <>
       <Navigation />
+      <Aside />
     </>
   );
 }
