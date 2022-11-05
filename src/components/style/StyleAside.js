b716f0481721ff5e8ac509230f6dfa1c8086bbc2
lDrex1 <damzealwini7@gmail.com>
2022-11-05 14:54:16 +0100

    new comp: Aside.js Styles

---
+++ src/components/style/StyleAside.js
@@ -0,0 +1,22 @@
+const styleAside = {
+  swiper: {
+    background: "",
+    border: "1px solid #fff",
+  },
+  box: {
+    background: "#222222",
+    width: "auto",
+    height: "100%",
+    color: "whitesmoke",
+    display: "flex",
+    alignItems: "center",
+  },
+  itemButton: {
+    borderRadius: "8px",
+    padding: "4px",
+    background: "green",
+    textAlign: "center",
+  },
+};
+
+export default styleAside;
