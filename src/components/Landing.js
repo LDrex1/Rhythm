3d064a9de6fea98c56d41978707721cd9b6634aa
lDrex1 <damzealwini7@gmail.com>
2022-11-14 08:20:57 +0100

    added: Box component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,17 @@
+import { Box } from "@mui/material";
 import React from "react";
 import Aside from "./aside/Aside";
 import LeftContent from "./mainContent/LeftContent";
 import Navigation from "./navigation/Navigation";
+import style from "./style/Style";
 
 function Landing() {
   return (
-    <>
+    <Box>
       <Navigation />
       <Aside />
       <LeftContent />
-    </>
+    </Box>
   );
 }
 
