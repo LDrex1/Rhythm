f027309521a801ebf5b545fe4d3bcd0385c51b8f
lDrex1 <damzealwini7@gmail.com>
2022-11-14 15:36:59 +0100

    rem Comp: Container and Grid Container

--- src/components/mainContent/LeftContent.js
+++ src/components/mainContent/LeftContent.js
@@ -1,22 +1,15 @@
 import React from "react";
-import { Container, Grid, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import styleMain from "../style/StyleMain";
 
 function LeftContent() {
   return (
-    <Container maxWidth="lg" sx={{ background: "yellow" }}>
-      <Grid container>
-        <Grid item md={6} sx={{ background: "#222222" }}>
-          <Typography
-            component="h1"
-            variant="h1"
-            sx={{ textTransform: "uppercase" }}
-          >
-            Welcome to the 3d world
-          </Typography>
-        </Grid>
-        <Grid item md={6}></Grid>
-      </Grid>
-    </Container>
+    <Grid item md={6}>
+      <Typography component="h1" variant="h1" sx={styleMain.leftContent.header}>
+        Welcome to the 3d world
+      </Typography>
+    </Grid>
+    // <Grid item md={6} sx={{ background: "yellow" }}></Grid>
   );
 }
 
