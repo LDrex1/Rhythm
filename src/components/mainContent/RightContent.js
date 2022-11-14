9c60cee3b5368630e3ed2ea8f273be75d9e2043a
lDrex1 <damzealwini7@gmail.com>
2022-11-14 15:38:01 +0100

    new:

---
+++ src/components/mainContent/RightContent.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import styleMain from "../style/StyleMain";
+
+function RightContent() {
+  return <Grid item md={6} sx={styleMain.rightContent}></Grid>;
+}
+
+export default RightContent;
