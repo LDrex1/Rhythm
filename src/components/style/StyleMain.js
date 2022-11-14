ca5e261bd384fe555a590213ed3013358e3f80ac
lDrex1 <damzealwini7@gmail.com>
2022-11-14 15:38:39 +0100

    new: information about styles for Main

---
+++ src/components/style/StyleMain.js
@@ -0,0 +1,9 @@
+const styleMain = {
+  leftContent: { header: { color: "#222245", textTransform: "uppercase" } },
+  rightContent: {
+    minHeight: { lg: "70vh" },
+    background: "yellow",
+  },
+};
+
+export default styleMain;
