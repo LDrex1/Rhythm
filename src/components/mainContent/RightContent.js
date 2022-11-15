a8b67c2111e4dc7da43e593ae5af5429b8dfc918
lDrex1 <damzealwini7@gmail.com>
2022-11-15 21:05:13 +0100

    update: Model style

--- src/components/mainContent/RightContent.js
+++ src/components/mainContent/RightContent.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import styleMain from "../style/StyleMain";
+import Model from "./canvas/Model";
 
 function RightContent() {
-  return <Grid item md={6} sx={styleMain.rightContent}></Grid>;
+  return (
+    <Grid item md={6} sx={styleMain.rightContent}>
+      <div style={{ height: "50vh" }}>
+        <Model />
+      </div>
+    </Grid>
+  );
 }
 
 export default RightContent;
