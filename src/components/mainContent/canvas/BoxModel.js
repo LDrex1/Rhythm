eef74c652e67b549428088422d3a509c0f562ba0
lDrex1 <damzealwini7@gmail.com>
2022-11-16 15:01:10 +0100

    added: uselayouteffect and line

--- src/components/mainContent/canvas/BoxModel.js
+++ src/components/mainContent/canvas/BoxModel.js
@@ -1,22 +1,41 @@
-import React, { useRef } from "react";
+import React, { useRef, useLayoutEffect } from "react";
 import { useFrame } from "@react-three/fiber";
-import { Mesh } from "three";
+import { Vector3 } from "three";
 
 function BoxModel() {
   const meshRef = useRef();
+  const lineRef = useRef();
 
   useFrame(() => {
     if (!meshRef.current) {
       return;
     }
-    meshRef.current.rotation.x += 0.01;
-    meshRef.current.rotation.y += 0.03;
+    meshRef.current.rotation.x += 0.005;
+    meshRef.current.rotation.y += 0.008;
+
+    if (!lineRef.current) {
+      return;
+    }
+    lineRef.current.rotation.x += 0.005;
+    lineRef.current.rotation.y += 0.008;
   });
+
+  const points = [new Vector3(-3, 0, 0), new Vector3(3, 0, 0)];
+
+  useLayoutEffect(() => {
+    lineRef.current.geometry.setFromPoints(points);
+  }, [points]);
   return (
-    <mesh ref={meshRef}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color={"#222245"} />
-    </mesh>
+    <>
+      <line ref={lineRef}>
+        <bufferGeometry />
+        <lineBasicMaterial color="black" />
+      </line>
+      <mesh ref={meshRef}>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial color={"#222245"} />
+      </mesh>
+    </>
   );
 }
 
