e17c1800471dcfc56c32c28ef75871587c0161cf
lDrex1 <damzealwini7@gmail.com>
2022-11-16 15:00:30 +0100

    update: box style

--- src/components/mainContent/RightContent.js
+++ src/components/mainContent/RightContent.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import styleMain from "../style/StyleMain";
 import Model from "./canvas/Model";
 
 function RightContent() {
   return (
     <Grid item md={6} sx={styleMain.rightContent}>
-      <div style={{ height: "50vh" }}>
+      <Box sx={{ height: { md: "50vh", lg: "70vh" } }}>
         <Model />
-      </div>
+      </Box>
     </Grid>
   );
 }
