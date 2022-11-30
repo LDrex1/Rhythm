91bef0f1c703e814f2b52baa791e2f8a520b1abd
lDrex1 <damzealwini7@gmail.com>
2022-11-30 13:51:31 +0100

    update: shadows and lights

--- src/components/mainContent/canvas/Model.js
+++ src/components/mainContent/canvas/Model.js
@@ -2,14 +2,24 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import BoxModel from "./BoxModel";
+import Surround from "./Surround";
+import PlaneA from "./planes/PlaneA";
 
 function Model() {
   return (
-    <Canvas>
+    <Canvas shadows>
       <ambientLight />
-      <pointLight position={[2, 10, 3]} />
+      {/* <pointLight position={[2, 10, 3]} /> */}
+      <spotLight
+        args={["#ffff67", 1.2, 13, 1.2, 0.4]}
+        position={[0, 4, 0.2]}
+        castShadow
+      />
       <BoxModel />
       <OrbitControls />
+      <axesHelper args={[5]} />
+      <Surround />
+      <PlaneA />
     </Canvas>
   );
 }
