97cc37c2afe3318753b70b995e75f37288aea9d1
lDrex1 <damzealwini7@gmail.com>
2022-12-01 22:33:44 +0100

    update: Texture added to enviroment

--- src/components/mainContent/canvas/Surround.js
+++ src/components/mainContent/canvas/Surround.js
@@ -1,13 +1,19 @@
-import { Environment } from "@react-three/drei";
+import { Environment, GradientTexture } from "@react-three/drei";
 import React from "react";
 import { BackSide } from "three";
 
 function Surround() {
   return (
     <Environment background>
-      <mesh scale={15}>
-        <sphereGeometry args={[50, 100, 100]} />
-        <meshBasicMaterial color={"green"} side={BackSide} />
+      <mesh scale={0.1}>
+        <boxGeometry args={[50, 100, 100]} />
+
+        <meshBasicMaterial side={BackSide}>
+          <GradientTexture
+            stops={[0, 0.5, 1]}
+            colors={["#ffff45", "skyblue", "blue"]}
+          />
+        </meshBasicMaterial>
       </mesh>
     </Environment>
   );
