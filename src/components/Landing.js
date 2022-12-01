c9379254994d44deaac0c5824281e2e62567842b
lDrex1 <damzealwini7@gmail.com>
2022-12-01 22:30:37 +0100

    updated: BigModel comp added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,10 +3,12 @@ import { Box } from "@mui/material";
 import Aside from "./aside/Aside";
 import Main from "./mainContent/Main";
 import Navigation from "./navigation/Navigation";
+import BigModel from "./mainContent/full_page_canvas/BigModel";
 
 function Landing() {
   return (
     <Box>
+      <BigModel />
       <Navigation />
       <Aside />
       <Main />
