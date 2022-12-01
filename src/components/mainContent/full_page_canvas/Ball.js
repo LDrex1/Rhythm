b00e028276770fd9f1a6218f77ce3702884ec1b7
lDrex1 <damzealwini7@gmail.com>
2022-12-01 22:34:25 +0100

    new comp for setting up ball

---
+++ src/components/mainContent/full_page_canvas/Ball.js
@@ -0,0 +1,23 @@
+import { useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { Vector3 } from "three";
+
+function Ball({ ballRadius, ballPosition }) {
+  const sphereRef = useRef();
+
+  useFrame(() => {
+    if (!!sphereRef.current) {
+      return;
+    }
+    sphereRef.current.rotation.x += 0.001;
+    sphereRef.current.rotation.x += 0.001;
+  });
+  return (
+    <mesh ref={sphereRef} position={new Vector3(...ballPosition)}>
+      <sphereBufferGeometry args={[ballRadius, 35, 35]} />
+      <meshStandardMaterial color={"skyblue"} />
+    </mesh>
+  );
+}
+
+export default Ball;
