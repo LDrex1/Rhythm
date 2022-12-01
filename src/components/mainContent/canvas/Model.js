93515e3cd5e331e025763c857fae54a88b3d6d47
lDrex1 <damzealwini7@gmail.com>
2022-12-01 22:32:55 +0100

    update: data and state for canvas dynamic changes

--- src/components/mainContent/canvas/Model.js
+++ src/components/mainContent/canvas/Model.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import BoxModel from "./BoxModel";
 import Surround from "./Surround";
 import PlaneA from "./planes/PlaneA";
+import DatGu from "../../gui/DatGui";
 
 function Model() {
+  const defaultData = {
+    light: { ambient: "yellow", spot: { color: "yellow", angle: "0.5" } },
+    enviroment: { color: "green" },
+    box: { color: "#234321", size: 2 },
+    heart: { size: 3, color: "#23232" },
+  };
+  const [datData, setDatData] = useState(defaultData);
   return (
-    <Canvas shadows>
-      <ambientLight />
-      {/* <pointLight position={[2, 10, 3]} /> */}
-      <spotLight
-        args={["#ffff67", 1.2, 13, 1.2, 0.4]}
-        position={[0, 4, 0.2]}
-        castShadow
-      />
-      <BoxModel />
-      <OrbitControls />
-      <axesHelper args={[5]} />
-      <Surround />
-      <PlaneA />
-    </Canvas>
+    <>
+      <Canvas shadows>
+        <ambientLight args={[datData.light.ambient]} />
+        {/* <pointLight position={[2, 10, 3]} /> */}
+        <spotLight
+          args={[datData.light.spot.color, 1.2, 13, 1.2, 0.4]}
+          position={[0, 4, 0.2]}
+          castShadow
+        />
+        <BoxModel />
+        <OrbitControls />
+        <axesHelper args={[5]} />
+        <Surround />
+        <PlaneA />
+      </Canvas>
+      <div style={{ position: "static", marginTop: "20px" }}>
+        <DatGu datData={datData} setDatData={setDatData} />
+      </div>
+    </>
   );
 }
 
