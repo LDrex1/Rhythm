7c8305758aae19cd0cb6808774798d80e7d6bc6b
lDrex1 <damzealwini7@gmail.com>
2022-12-04 20:46:09 +0100

    Box, Typography and Link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Box, Link, Typography } from "@mui/material";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Box
+      sx={{
+        position: "absolute",
+        width: "100%",
+        bottom: "4px",
+        top: "auto",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        columnGap: "5px",
+      }}
+    >
+      <Typography sx={{ fontWeight: "650" }} component={"p"} vatiant={"h6"}>
+        Created by
+      </Typography>
+      <Link
+        sx={{ textDecoration: "none" }}
+        variant="h6"
+        href="https://github.com/LDrex1"
+      >
+        LDrex1
+      </Link>
+    </Box>
+  );
 }
 
 export default Footer;
