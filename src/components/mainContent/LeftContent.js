c6024c8df49950eec428f0ccde3856c05c834db2
lDrex1 <damzealwini7@gmail.com>
2022-12-04 20:46:32 +0100

    z-index

--- src/components/mainContent/LeftContent.js
+++ src/components/mainContent/LeftContent.js
@@ -4,7 +4,7 @@ import styleMain from "../style/StyleMain";
 
 function LeftContent() {
   return (
-    <Grid item md={6}>
+    <Grid item md={6} sx={styleMain.leftContent.grid}>
       <Typography component="h1" variant="h1" sx={styleMain.leftContent.header}>
         Welcome to the 3d world
       </Typography>
