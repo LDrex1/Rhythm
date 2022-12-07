207a8ee5fcad3a47258324efbe3d4f818c9e8a34
lDrex1 <damzealwini7@gmail.com>
2022-12-07 19:01:26 +0100

    update: orbitControls

--- src/components/mainContent/full_page_canvas/BigModel.js
+++ src/components/mainContent/full_page_canvas/BigModel.js
@@ -1,16 +1,30 @@
+import React from "react";
 import { Box } from "@mui/material";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React from "react";
 import Balls from "./Balls";
+import Points from "./Points";
 
 function BigModel() {
   return (
-    <Box sx={{ position: "fixed", top: "0", left: "0" }}>
+    <Box
+      sx={{
+        position: "fixed",
+        top: "0",
+        left: "0",
+        height: "100%",
+        width: "100%",
+      }}
+    >
       <Canvas>
+        <Points />
         <Balls />
-        <ambientLight color={"#ffffff"} />
-        <OrbitControls />
+        <ambientLight intensity={1} color={"#ffffff"} />
+        <OrbitControls
+          enableRotate={true}
+          autoRotate={false}
+          autoRotateSpeed={0.6}
+        />
         <axesHelper args={[4]} />
       </Canvas>
     </Box>
