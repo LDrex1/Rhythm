45348e70ddf46dbae6b88a50989f6591185abb1d
lDrex1 <damzealwini7@gmail.com>
2022-12-09 23:34:38 +0100

    update: style

--- src/components/mainContent/LeftContent.js
+++ src/components/mainContent/LeftContent.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Grid, Typography } from "@mui/material";
 import styleMain from "../style/StyleMain";
+import CustomThemeProvider from "../customThemes/CustomThemeProvider";
 
 function LeftContent() {
   return (
     <Grid item md={6} sx={styleMain.leftContent.grid}>
-      <Typography component="h1" variant="h1" sx={styleMain.leftContent.header}>
-        Welcome to the 3d world
-      </Typography>
+      <CustomThemeProvider>
+        <Typography
+          component="h1"
+          variant="h1"
+          align="center"
+          textTransform={"uppercase"}
+          sx={styleMain.leftContent.header}
+        >
+          Welcome to the 3d world
+        </Typography>
+      </CustomThemeProvider>
     </Grid>
   );
 }
