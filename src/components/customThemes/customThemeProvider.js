06e83979407add2fd3ad6fa8739acf5d5d438985
lDrex1 <damzealwini7@gmail.com>
2022-12-10 00:21:49 +0100

    update: ThemeProvider

--- src/components/customThemes/customThemeProvider.js
+++ src/components/customThemes/customThemeProvider.js
@@ -1,23 +1,26 @@
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import React, { Children } from "react";
+import React from "react";
 
 const theme = createTheme();
 
 theme.typography.h1 = {
   fontSize: "2.2rem",
+  fontFamily: `-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+  "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+  sans-serif`,
+  [theme.breakpoints.up("sm")]: {
+    fontSize: "3rem",
+  },
   [theme.breakpoints.up("md")]: {
-    fontSize: "2.4rem",
+    fontSize: "3.7rem",
   },
   [theme.breakpoints.up("lg")]: {
-    fontSize: "2.6rem",
-  },
-  [theme.breakpoints.up("xl")]: {
-    fontSize: "2.83rem",
+    fontSize: "6rem",
   },
 };
 
-function customThemeProvider() {
-  return <ThemeProvider theme={theme}>{Children}</ThemeProvider>;
+function CustomThemeProvider({ children }) {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
 
-export default customThemeProvider;
+export default CustomThemeProvider;
