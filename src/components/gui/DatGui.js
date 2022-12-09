d6eb5ea9899a043c3eac03ff8c6edbcf1f620ac2
lDrex1 <damzealwini7@gmail.com>
2022-12-09 23:33:56 +0100

    update: style

--- src/components/gui/DatGui.js
+++ src/components/gui/DatGui.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import DatGui, { DatBoolean, DatColor } from "react-dat-gui";
 import "../../../node_modules/react-dat-gui/dist/index.css";
 
 function DatGu({ datData, setDatData }) {
+  const guiRef = useRef();
+
   const handleGuiUpdate = (newData) => {
     setDatData((prevData) => {
       return { ...prevData, ...newData };
     });
   };
 
+  console.log(guiRef);
+
+  console.log(DatGui);
   return (
-    <DatGui data={datData} onUpdate={handleGuiUpdate}>
+    <DatGui
+      ref={guiRef}
+      style={{ position: "absolute" }}
+      data={datData}
+      onUpdate={handleGuiUpdate}
+    >
       <DatBoolean />
       <DatColor path="enviroment/color" label="enviroment" />
       <DatColor path="light.ambient" label="ambientLight" />
