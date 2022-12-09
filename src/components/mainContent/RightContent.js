b28b669472a8a06b8b8bb45115d44fc824eb7de8
lDrex1 <damzealwini7@gmail.com>
2022-12-09 23:34:47 +0100

    update: style

--- src/components/mainContent/RightContent.js
+++ src/components/mainContent/RightContent.js
@@ -5,13 +5,8 @@ import Model from "./canvas/Model";
 
 function RightContent() {
   return (
-    <Grid item md={6} sx={styleMain.rightContent}>
-      <Box
-        sx={{
-          height: { sm: "60vh", md: "50vh", lg: "70vh" },
-          borderRadius: "15px",
-        }}
-      >
+    <Grid item md={6}>
+      <Box sx={styleMain.rightContent}>
         <Model />
       </Box>
     </Grid>
