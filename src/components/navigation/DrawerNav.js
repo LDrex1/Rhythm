1fcd4b399a9016ae2695ac8c60ece4a39d26a164
lDrex1 <damzealwini7@gmail.com>
2022-12-11 23:04:53 +0100

    update: optimisation

--- src/components/navigation/DrawerNav.js
+++ src/components/navigation/DrawerNav.js
@@ -1,17 +1,7 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Stack,
-} from "@mui/material";
-import { styleNav } from "../style/StyleNav";
-import NavLinks from "./NavLinks";
+import React from "react";
+import { Box, Drawer, List, Stack } from "@mui/material";
 import LinkList from "./LinkList";
+import { Center } from "@react-three/drei";
 
 function DrawerNav(props) {
   const drawerWidth = 150;
@@ -20,11 +10,28 @@ function DrawerNav(props) {
     setMobileOpen(() => !mobileOpen);
   };
 
-  const { window } = props;
   const container =
-    window !== undefined ? () => window().document.body : undefined;
+    window !== undefined ? document.querySelector(".page") : undefined;
+  console.log(container);
+
+  // const useStyles = makeStyles({
+  //   drawer: {
+  //     position: "relative",
+  //     marginLeft: "auto",
+  //     width: 200,
+  //     "& .MuiBackdrop-root": {
+  //       display: "none",
+  //     },
+  //     "& .MuiDrawer-paper": {
+  //       width: 200,
+  //       position: "absolute",
+  //       height: "80vh",
+  //       transition: "none !important",
+  //     },
+  //   },
+  // });
   return (
-    <Box component={"nav"}>
+    <Box component={"nav"} sx={{ display: "flex", alignItems: "center" }}>
       <Drawer
         container={container}
         variant={"temporary"}
@@ -34,8 +41,17 @@ function DrawerNav(props) {
           keepMounted: true,
         }}
         sx={{
-          display: { xs: "block", md: "none" },
-          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+          display: { xs: "flex", md: "none" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            justifyContent: "center",
+            width: drawerWidth,
+            background: "none",
+            boxShadow: "none",
+            height: "60vh",
+            background: "#222222",
+            borderRadius: "0 0 8px 0",
+          },
         }}
       >
         <Stack onClick={handleDrawerToggle}>
