8818409045fdf0514c985aeeb66c05b1e23cba5e
lDrex1 <damzealwini7@gmail.com>
2022-12-12 23:39:20 +0100

    update: dynamic Grid column change

--- src/components/mainContent/RightContent.js
+++ src/components/mainContent/RightContent.js
@@ -3,9 +3,18 @@ import { Box, Grid } from "@mui/material";
 import styleMain from "../style/StyleMain";
 import Model from "./canvas/Model";
 
-function RightContent() {
+function RightContent({ showCanvas }) {
   return (
-    <Grid item md={6}>
+    <Grid
+      item
+      md={showCanvas ? 6 : 0}
+      xs={showCanvas ? 12 : 0}
+      sx={{
+        maxWidth: showCanvas ? "100%" : "0 !important",
+        overflowX: "hidden",
+        transition: { md: "all 0.5s ease-out", xs: "all 0.5s ease-out" },
+      }}
+    >
       <Box sx={styleMain.rightContent}>
         <Model />
       </Box>
