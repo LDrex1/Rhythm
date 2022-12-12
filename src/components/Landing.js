6c36a047ff0bdb18e988f98b997de7cb9e362fe9
lDrex1 <damzealwini7@gmail.com>
2022-12-12 23:45:32 +0100

    cleaning

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,6 @@ import Main from "./mainContent/Main";
 import Navigation from "./navigation/Navigation";
 import BigModel from "./mainContent/full_page_canvas/BigModel";
 import Footer from "./footer/Footer";
-import CustomThemeProvider from "./customThemes/CustomThemeProvider";
 
 function Landing() {
   return (
