8d357a3b8cc66c7ff8fc36bc9fbb6bbaa7a3349b
lDrex1 <damzealwini7@gmail.com>
2022-12-14 19:54:24 +0100

    update: icon color

--- src/components/aside/AsideLinks.js
+++ src/components/aside/AsideLinks.js
@@ -1,26 +1,31 @@
 import React from "react";
 import { Divider, List, ListItem } from "@mui/material";
-import { ListItemButton, ListItemIcon, ListItemText, Box } from "@mui/material";
+import { ListItemButton, ListItemIcon, Box } from "@mui/material";
 import { Facebook, Instagram, Twitter, YouTube } from "@mui/icons-material";
 import styleAside from "../style/StyleAside";
 
 const asideLinks = [
-  { label: "Facebook", icon: <Facebook />, url: "#" },
-  { label: "Instagram", icon: <Instagram />, url: "#" },
-  { label: "YouTube", icon: <YouTube />, url: "#" },
-  { label: "Twitter", icon: <Twitter />, url: "#" },
+  {
+    label: "Facebook",
+    icon: <Facebook />,
+    url: "#",
+    color: "#4267b2",
+  },
+  { label: "Instagram", icon: <Instagram />, url: "#", color: "#d62976" },
+  { label: "YouTube", icon: <YouTube />, url: "#", color: "#ff0000" },
+  { label: "Twitter", icon: <Twitter />, url: "#", color: "#1da1f2" },
 ];
 
+console.log(asideLinks[0].icon);
 const ListBox = () => (
   <>
     <Box sx={styleAside.box}>
       <List>
-        {asideLinks.map(({ label, icon, url }, index) => (
+        {asideLinks.map(({ label, icon, url, color }, index) => (
           <>
             <ListItem key={label}>
               <ListItemButton LinkComponent="a" href={url}>
-                <ListItemIcon sx={{ color: "white" }}>{icon}</ListItemIcon>
-                <ListItemText primary={label} sx={styleAside.itemButton} />
+                <ListItemIcon sx={{ color: color }}>{icon}</ListItemIcon>
               </ListItemButton>
             </ListItem>
             {index !== asideLinks.length - 1 ? (
