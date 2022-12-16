d4d79ad022bfd3ca7d8958f96d24b2de3903779b
lDrex1 <damzealwini7@gmail.com>
2022-12-17 00:26:29 +0100

    update

--- src/components/style/StyleAside.js
+++ src/components/style/StyleAside.js
@@ -2,21 +2,26 @@ const styleAside = {
   swiper: {
     background: "",
     border: "1px solid #fff",
+    "& .MuiDrawer-paper": {
+      width: 70,
+      background: "none",
+      boxShadow: "none",
+    },
   },
   box: {
-    background: "#222222",
-    width: "auto",
+    background: "none",
+    // width: "auto",
     height: "100%",
-    color: "whitesmoke",
+    // color: "whitesmoke",
     display: "flex",
     alignItems: "center",
   },
-  itemButton: {
-    borderRadius: "8px",
-    padding: "4px",
-    background: "green",
-    textAlign: "center",
-  },
+  // itemButton: {
+  //   borderRadius: "8px",
+  //   padding: "4px",
+  //   background: "green",
+  //   textAlign: "center",
+  // },
 };
 
 export default styleAside;
