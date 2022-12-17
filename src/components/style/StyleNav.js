a0982852507ef9eff88e19c6b6e3aecdd324f161
lDrex1 <damzealwini7@gmail.com>
2022-12-17 10:07:14 +0100

    added: link pseudo styles

--- src/components/style/StyleNav.js
+++ src/components/style/StyleNav.js
@@ -3,11 +3,12 @@ export const styleNav = {
     display: "flex",
     justifyContent: "space-between",
   },
-  toolbar: {
-    width: "60%",
-    display: "flex",
-    justifyContent: "space-between",
-  },
+  // toolbar: {
+  //   width: "60%",
+  //   display: "flex",
+  //   justifyContent: "space-between",
+  // },
+  item: {},
   link: {
     background: "linear-gradient( #222245 80%, #888888 )",
     borderRadius: "5px",
@@ -15,5 +16,29 @@ export const styleNav = {
     paddingBottom: "6px",
     textDecoration: "none",
     color: "whitesmoke",
+
+    "&:hover": {
+      background: "none",
+      color: "#222222",
+    },
+
+    "&::before": {
+      position: "absolute",
+      content: '" "',
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "0",
+      transition: "all 300ms ease-out ",
+      background: "linear-gradient(#888888 80%, #222245)",
+      color: "#111111",
+      zIndex: -1,
+      opacity: 0,
+    },
+
+    "&:hover::before": {
+      height: "100%",
+      opacity: 1,
+    },
   },
 };
