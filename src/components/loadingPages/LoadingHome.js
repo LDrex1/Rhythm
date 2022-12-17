0b9d394e1ecd0a7f9e3499119a5f0569d60e9eab
lDrex1 <damzealwini7@gmail.com>
2022-12-17 10:05:56 +0100

    added: loading page style

--- src/components/loadingPages/LoadingHome.js
+++ src/components/loadingPages/LoadingHome.js
@@ -8,15 +8,16 @@ function LoadingHome() {
         position: "absolute",
         background: "#222222",
         height: "100vh",
-        color: "green",
+        color: "#222245",
         width: "100%",
+        zIndex: 10,
       }}
       flex
       alignItems={"center"}
       justifyContent={"center"}
     >
       <Typography align={"center"} component={"h2"} variant={"h2"}>
-        ...Loading
+        Loading...
       </Typography>
     </Box>
   );
