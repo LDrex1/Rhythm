19a71b27d04c2d3f192dd64d3cd69d86702eb2a1
lDrex1 <damzealwini7@gmail.com>
2022-12-17 10:06:43 +0100

    new: InfoOutlined icon

--- src/components/navigation/NavLinks.js
+++ src/components/navigation/NavLinks.js
@@ -2,13 +2,14 @@ import {
   Build,
   ConnectWithoutContact,
   Home,
+  InfoOutlined,
   QuestionMark,
 } from "@mui/icons-material";
 
 const NavLinks = [
   { title: "Home", url: "#", icon: <Home /> },
   { title: "Tech", url: "#", icon: <Build /> },
-  { title: "About", url: "#" },
+  { title: "About", url: "#", icon: <InfoOutlined /> },
   { title: "Contact", url: "#", icon: <ConnectWithoutContact /> },
   { title: "Help", url: "#", icon: <QuestionMark /> },
 ];
